528ee47d799d2ccd0ef294b4abadbd31d1c459cc
rfischback <ryan-fischback@hotmail.com>
2024-08-22 23:19:15 -0300

    project init

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+// Render the App component inside React.StrictMode
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
